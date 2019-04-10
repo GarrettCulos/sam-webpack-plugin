cd7005c7beb81910feb6a195c586d778bda32ca4
garrett <garrettculos@gmail.com>
2019-04-09 20:00:48 -0700

    Update .spec file, converage requirements, and add test data

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,44 +2,59 @@ import { SamWebpackPlugin } from './index';
 
 describe('sanity checks', () => {
   it('export is valid class', () => {
-    expect(typeof SamWebpackPlugin).toEqual('class');
+    const classTest = new SamWebpackPlugin({ baseTemplate: './test/data/testingConfig.json' });
+    expect(classTest instanceof SamWebpackPlugin).toBeTruthy();
   });
 });
 
 describe('constructor defines variables and throws errors', () => {
   it("plugin constructor defines regex's", () => {
-    const SamPlugin = new SamWebpackPlugin({ baseTemplate: './testing.yml' });
+    const SamPlugin = new SamWebpackPlugin({ baseTemplate: './test/data/testingConfig.json' });
     expect(SamPlugin.declarationRegex).toBeDefined();
     expect(SamPlugin.declarationRegex).toEqual(/(?<=@WebpackLambda\()([^\)]+)(?=(\)))/g);
   });
 
   it('plugin constructor defines deployFolder', () => {
-    const SamPlugin = new SamWebpackPlugin({ baseTemplate: './testing.yml' });
+    const SamPlugin = new SamWebpackPlugin({ baseTemplate: './test/data/testingConfig.json' });
     expect(SamPlugin.deploymentFolder).toBeDefined();
     expect(SamPlugin.deploymentFolder).toEqual('./lambda-sam-deploy');
 
-    const SamPlugin2 = new SamWebpackPlugin({ output: 'new-output', baseTemplate: './testing.yml' });
+    const SamPlugin2 = new SamWebpackPlugin({ output: 'new-output', baseTemplate: './test/data/testingConfig.json' });
     expect(SamPlugin2.deploymentFolder).toEqual('new-output');
     // TODO figure out how to test error handling
-    // expect(new SamWebpackPlugin({ output: <string>23423, baseTemplate: './testing.yml' })).toThrowError(
+    // expect(new SamWebpackPlugin({ output: <string>23423, baseTemplate: './testing.json' })).toThrowError(
     //   '[SamWEepackPlugin]: options.output must be of type String'
     // );
   });
 
-  it('plugin constructor defines options', () => {
-    const SamPlugin = new SamWebpackPlugin({ baseTemplate: './testing.yml' });
-    expect(SamPlugin.deploymentFolder).toBeDefined();
-    expect(SamPlugin.deploymentFolder).toEqual('./lambda-sam-deploy');
-
-    const SamPlugin2 = new SamWebpackPlugin({ output: 'newoutput', baseTemplate: './testing.yml' });
-    expect(SamPlugin2.options).toBeDefined();
+  it('plugin constructor defines options.verbose', () => {
+    const SamPlugin = new SamWebpackPlugin({ baseTemplate: './test/data/testingConfig.json' });
+    expect(SamPlugin.options).toBeDefined();
     expect(SamPlugin.options.verbose).toBeDefined();
-    expect(SamPlugin.options.verbose).toEqual('');
+    expect(SamPlugin.options.verbose).toBeFalsy();
+
+    const SamPlugin2 = new SamWebpackPlugin({ verbose: true, baseTemplate: './test/data/testingConfig.json' });
+    expect(SamPlugin2.options).toBeDefined();
+    expect(SamPlugin2.options.verbose).toBeDefined();
+    expect(SamPlugin2.options.verbose).toBeTruthy();
   });
+
+  it('plugin constructor defines options.requireText', () => {
+    const SamPlugin = new SamWebpackPlugin({ baseTemplate: './test/data/testingConfig.json' });
+    expect(SamPlugin.options).toBeDefined();
+    expect(SamPlugin.options.requireTxt).toBeDefined();
+    expect(SamPlugin.options.requireTxt).toBeFalsy();
+
+    const SamPlugin2 = new SamWebpackPlugin({ requireTxt: true, baseTemplate: './test/data/testingConfig.json' });
+    expect(SamPlugin2.options).toBeDefined();
+    expect(SamPlugin2.options.requireTxt).toBeDefined();
+    expect(SamPlugin2.options.requireTxt).toBeTruthy();
+  });
+  it('plugin constructor should save layers options', () => {});
 });
 
 describe('parseLambdaDeclaration', () => {
-  it('parseLambdaDeclaration should be defined');
+  it('parseLambdaDeclaration should be defined', () => {});
   it("parseLambdaDeclaration should return when string doesn't contain @lambdaDeclaration", () => {});
   it('parseLambdaDeclaration should parse string to json when it contains @lambdaDeclaration', () => {});
   it('parseLambdaDeclaration should throw error when content is not a json', () => {});
